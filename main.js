72a88d3829e23b3cdac506740264b56401c2cbd4
vsadhuvenkata <82234818+vsadhuvenkata@users.noreply.github.com>
2021-07-01 10:11:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menu_list_array = ["Peppy Paneer","Mexican Green Wave","Deluxe Veggie","Non Veg Supreime","Pepper Barbecue and Onion","Chicken Dominator"]
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>";
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '<li>';
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>";
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">'+'<img src="images/pizzaImg.png" style="width: 100px; height:80px;"/>'+menu_list_array[i]+'</div';
+}
+htmldata=htmldata+"</section>"
+document.getElementById("display_addmenu").innerHTML = htmldata;
+}
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    add_item();
+
+}
